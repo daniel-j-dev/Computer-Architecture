75899f6e921a8c872839e6494f721ab17f02f62e
iDecisive <danielaugustusjohnson@gmail.com>
2020-09-16 15:55:52 -0400

    mult.ls8 works

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,28 +7,64 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.running = True
+        self.pc = 0
+        self.ram = [None]*256
+        self.reg = [None]*8
+        self.appsPath = './apps/'
 
     def load(self):
         """Load a program into memory."""
 
+        #listing apps to load
+
+        import os
+
+        for file in os.listdir(self.appsPath):
+            if os.path.isfile(os.path.join(self.appsPath, file)):
+                print(file)
+
+        #user choosing app to load
+
+        text = input('\nchoose an app to execute...\n')
         address = 0
+
+        try:
+            with open(f'{self.appsPath}{text}') as f:
+                for line in f:
+                    if line == '':
+                        continue
+
+                    try:
+                        line = int(line, 2)
+                    except ValueError:
+                        print(f'Invalid instuction: {line} ')
+                        sys.exit(1)
+
+                    self.ram_write(address, line)
+                    address += 1
+                    
+        except FileNotFoundError:
+            print(f'File not found: {text}')
+            sys.exit(2)
+
+        #address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -37,8 +73,16 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == 'MUL':
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def trace(self):
         """
@@ -62,4 +106,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        #instruction definitions
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        #run code
+        while self.running:
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+            if ir == HLT:
+                print('shutting down...')
+                self.running = False
+            elif ir == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif ir == MUL:
+                self.alu('MUL', operand_a, operand_b )
+                self.pc += 3
+            else:
+                print('invalid instruction')
+                self.pc += 1
+
